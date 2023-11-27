645a5b0e3c3e698e4a98c68e53cb246ef6784f4d
ZenOkami <gianstasi@yahoo.com>
2023-11-26 18:54:03 -0700

    Enzyme Application Commit

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,12 +1,9 @@
 import React from "react";
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from "enzyme";
+import toJSON from 'enzyme-to-json'
 import Header from '../../components/headerPage'
 
 test('Should Render Header Correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
-
-    console.log(renderer.getRenderOutput());
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
 })
