bea16dfef51f01afb7ae3f96c4f6592245cc22ea
ZenOkami <gianstasi@yahoo.com>
2024-03-21 00:57:41 -0600

    Randomized Buttons on Quiz Game 📝

--- Quiz-Game/src/components/Quiz.jsx
+++ Quiz-Game/src/components/Quiz.jsx
@@ -14,6 +14,18 @@ const quizQuestions = [
         wrongAnswer: "Red",
         wrongAnswer2: "Green",
         wrongAnswer3: "Yellow"
+    }, {
+        question: "Who is Batman?",
+        answer: "Bruce Wayne",
+        wrongAnswer: "John",
+        wrongAnswer2: "Joker",
+        wrongAnswer3: "Pikachu"
+    }, {
+        question: "What is React a framework for?",
+        answer: "JavaScript",
+        wrongAnswer: "Python",
+        wrongAnswer2: "Rust",
+        wrongAnswer3: "C++"
     }
 ]
 
@@ -21,28 +33,47 @@ const Quiz = () => {
     let i = 0;
     const [count, setCount] = useState(0);
     const [index, setIndex] = useState(0);
+    const [buttonOrder, setButtonOrder] = useState([1, 2, 3, 4].sort(() => Math.random() - 0.5));
+    
     const questionNumber = quizQuestions.findIndex(obj => obj.answer === quizQuestions[index].answer) + 1;
+
     const handleClick = (e) => {
         console.log(e.target.value, index, count);
+        setButtonOrder(buttonOrder.sort(() => Math.random() - 0.5))
+        console.log(buttonOrder);
+        
         setIndex((index) => ++index);
         if (index === quizQuestions.length - 1) {
             setIndex((index) => index = 0);
         }
-
-        if (count === quizQuestions.length) {
-            alert("Congratulations! You got all questions correctly!");
-        }
         
-        return e.target.value === quizQuestions[index].answer && setCount((count) => ++count)
+        return e.target.value === quizQuestions[index].answer && setCount((count) => ++count);
     };
+
+    if (index === quizQuestions.length) {
+        setIndex((index) => index = 0);
+    }
+
+    let wonGame = false;
+
+    if (count === (quizQuestions.length) && !wonGame) {
+        console.log(wonGame);
+        alert("Congratulations! You got all questions correctly!");
+        wonGame = true;
+        console.log(wonGame);
+    }
     
+    const CorrectAnswer = () => (<button type="button" onClick={handleClick} value={quizQuestions[index].answer}>{quizQuestions[index].answer}</button>);
+    const WrongAnswer1 = () => (<button type="button" onClick={handleClick} value={quizQuestions[index].wrongAnswer}>{quizQuestions[index].wrongAnswer}</button>);
+    const WrongAnswer2 = () => (<button type="button" onClick={handleClick} value={quizQuestions[index].wrongAnswer2}>{quizQuestions[index].wrongAnswer2}</button>);
+    const WrongAnswer3 = () => (<button type="button" onClick={handleClick} value={quizQuestions[index].wrongAnswer3}>{quizQuestions[index].wrongAnswer3}</button>);
     return (
     <div>
-        <h3>Question {questionNumber}. {quizQuestions[i].question}</h3>
-        <button type="button" onClick={handleClick} value={quizQuestions[index].answer}>{quizQuestions[index].answer}</button>
-        <button type="button" onClick={handleClick} value={quizQuestions[index].wrongAnswer}>{quizQuestions[index].wrongAnswer}</button><br/>
-        <button type="button" onClick={handleClick} value={quizQuestions[index].wrongAnswer2}>{quizQuestions[index].wrongAnswer2}</button>
-        <button type="button" onClick={handleClick} value={quizQuestions[index].wrongAnswer3}>{quizQuestions[index].wrongAnswer3}</button>
+        <h3>Question {questionNumber}. {quizQuestions[index].question}</h3>
+        {buttonOrder[0] === 1 ? <CorrectAnswer /> : buttonOrder[0] === 2 ? <WrongAnswer1 /> : buttonOrder[0] === 3 ? <WrongAnswer2 /> : <WrongAnswer3 />}
+        {buttonOrder[1] === 1 ? <CorrectAnswer /> : buttonOrder[1] === 2 ? <WrongAnswer1 /> : buttonOrder[1] === 3 ? <WrongAnswer2 /> : <WrongAnswer3 />}<br/>
+        {buttonOrder[2] === 1 ? <CorrectAnswer /> : buttonOrder[2] === 2 ? <WrongAnswer1 /> : buttonOrder[2] === 3 ? <WrongAnswer2 /> : <WrongAnswer3 />}
+        {buttonOrder[3] === 1 ? <CorrectAnswer /> : buttonOrder[3] === 2 ? <WrongAnswer1 /> : buttonOrder[3] === 3 ? <WrongAnswer2 /> : <WrongAnswer3 />}
         <p>Score: {count}</p>
     </div>)
 }
